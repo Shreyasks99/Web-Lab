73f2a30392913de8437d70292b50527e2a07d530
unknown <shreyasks99@gmail.com>
2020-12-03 12:52:32 +0530

    Lab Program 7 added

--- Lab Program 6/Lab Program 6.php
+++ Lab Program 6/Lab Program 6.php
@@ -9,8 +9,8 @@
         $counter =(int)fread($handle,20);
         fclose($handle);
         $counter++;
-        echo "<p>Welcome To the Page</p>
-              <p align=center><strong>You are visitor Number ". $counter . "</strong></p>"
+        echo "<h1 align=center>Welcome To the Page</h1>
+              <h1 align=center><strong>You are visitor Number ". $counter . "</strong></h1>"
         $handle = fopen("counter.txt","w");
         fwrite($handle,$counter);
         fclose($handle);
